4ba814fdde004e612efb87b1eeb207b709e3fc58
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-06-09 14:40:22 -0500

    Create cli.py

---
+++ entrypoint/entrypoint/cli.py
@@ -0,0 +1,28 @@
+import argparse
+
+def init(sys_argv=None) -> argparse.Namespace:
+    """
+    Initializes the CLI using argparse.
+    :param sys_argv: A list of arguments; you should pass sys.argv in most cases.
+    Alternatively, you can provide custom values for use in unit tests.
+    :return: namespace containing values for each CLI key
+    """
+    program_description = "Fetch Amazon Inspector EC2 scan results."
+    parser = argparse.ArgumentParser(description=program_description)
+
+    parser.add_argument('--aws-access-key-id', type=str, required=True,
+                        help='AWS Access Key ID')
+    parser.add_argument('--aws-secret-access-key', type=str, required=True,
+                        help='AWS Secret Access Key')
+    parser.add_argument('--aws-region', type=str, required=True,
+                        help='AWS Region')
+    parser.add_argument('--assessment-run-arn', type=str, required=True,
+                        help='Amazon Inspector Assessment Run ARN')
+
+    args = ""
+    if sys_argv:
+        args = parser.parse_args(sys_argv)
+    else:
+        args = parser.parse_args()
+
+    return args
