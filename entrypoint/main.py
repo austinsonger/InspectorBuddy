94e55bce9ae1edca7064519ce3b5302f049fc7b4
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-06-09 14:41:53 -0500

    Update main.py

--- entrypoint/main.py
+++ entrypoint/main.py
@@ -2,11 +2,15 @@
 
 import sys
 import boto3
-from entrypoint import cli
+from entrypoint import cli, installer
 
 def main():
     # Initialize CLI arguments
     args = cli.init(sys.argv[1:])
+
+    # Prepare the environment
+    if not installer.prepare_environment(args.aws_access_key_id, args.aws_secret_access_key, args.aws_region):
+        sys.exit(1)
 
     client = boto3.client(
         'inspector',
