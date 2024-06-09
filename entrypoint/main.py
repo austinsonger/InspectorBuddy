63a1a72303c25687890b5942b935e3f09814eef1
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-06-09 14:43:21 -0500

    Update main.py

--- entrypoint/main.py
+++ entrypoint/main.py
@@ -2,7 +2,7 @@
 
 import sys
 import boto3
-from entrypoint import cli, installer, log_conf
+from entrypoint import cli, installer, log_conf, orchestrator
 
 def main():
     # Initialize CLI arguments
@@ -15,15 +15,9 @@ def main():
     if not installer.prepare_environment(args.aws_access_key_id, args.aws_secret_access_key, args.aws_region):
         sys.exit(1)
 
-    client = boto3.client(
-        'inspector',
-        aws_access_key_id=args.aws_access_key_id,
-        aws_secret_access_key=args.aws_secret_access_key,
-        region_name=args.aws_region
-    )
-
-    findings = client.list_findings(assessmentRunArns=[args.assessment_run_arn])
-    print(f"Findings: {findings}")
+    # Execute the main orchestration
+    ret = orchestrator.execute(args)
+    sys.exit(ret)
 
 if __name__ == '__main__':
     main()
