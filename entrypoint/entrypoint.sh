77994ff0942c9d4a7043196a062804ba67d16f84
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-06-09 14:52:16 -0500

    Create entrypoint.sh

---
+++ entrypoint/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -l
+
+set -e
+
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+AWS_REGION=$3
+ASSESSMENT_RUN_ARN=$4
+
+# Configure AWS CLI
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set region "$AWS_REGION"
+
+# Run the main Python script
+python3 /path/to/your/main_script.py --aws-access-key-id "$AWS_ACCESS_KEY_ID" --aws-secret-access-key "$AWS_SECRET_ACCESS_KEY" --aws-region "$AWS_REGION" --assessment-run-arn "$ASSESSMENT_RUN_ARN"
